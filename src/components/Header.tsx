702c73f6f54a44f52df153c97e8b5451ab801a62
Guilherme <guilherme1067@hotmail.com>
2023-06-26 09:06:11 -0300

    feat: ended layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="flex flex-col gap-1">
+    <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+    <span>Módulo "Desmitificando o Redux"</span>
+  </div>
+  )
+}
+
